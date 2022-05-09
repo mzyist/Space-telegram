be475450c5adde0d60327e343b704030a35d69db
mzyist <mzyist@gmail.com>
2022-05-09 12:14:57 +0300

    Made links_amount and apod_amount global

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -12,7 +12,7 @@ def fetch_nasa_apod(nasa_directory, nasa_token, nasa_url):
     nasa_url = f'{nasa_url}planetary/apod'
     payload = {
         'api_key': nasa_token,
-        'count': 30
+        'count': apod_amount
     }
     response = requests.get(nasa_url, params=payload)
     response.raise_for_status()
@@ -29,7 +29,6 @@ def fetch_epic(epic_directory, nasa_token, nasa_url):
     epic_response.raise_for_status()
     epic_content = epic_response.json()
     image_url_template = '{}EPIC/archive/natural/{}/png/{}.png?api_key={}'
-    links_amount = 10
     for count, content in enumerate(epic_content[:links_amount]):
         date_string = content['date']
         image_name = content['image']
@@ -53,5 +52,7 @@ if __name__ == "__main__":
     pathlib.Path(epic_directory).mkdir(exist_ok=True)
     pathlib.Path(nasa_directory).mkdir(exist_ok=True)
     nasa_url = 'https://api.nasa.gov/'
+    links_amount = 10
+    apod_amount = 30
     fetch_epic(epic_directory, nasa_token, nasa_url)
     fetch_nasa_apod(nasa_directory, nasa_token, nasa_url)
