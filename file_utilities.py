8bdd5ca07beea37076f5b9f5dbe74814a8d63374
mzyist <mzyist@gmail.com>
2022-05-09 12:28:04 +0300

    Changed function name

--- file_utilities.py
+++ file_utilities.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 import requests
 
 
-def get_file_extension(file_path):
+def get_url_extension(file_path):
     parsed = urlparse(file_path)
     path = os.path.splitext(parsed[2])
     return path[1]
@@ -13,6 +13,6 @@ def get_file_extension(file_path):
 def save_image_file(directory, image_url, file_name):
     image_response = requests.get(image_url)
     image_response.raise_for_status()
-    extension = get_file_extension(image_url)
+    extension = get_url_extension(image_url)
     with open(f'{directory}{file_name}{extension}', 'wb') as file:
         file.write(image_response.content)
