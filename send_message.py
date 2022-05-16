263e28019565fe62e5a678a32daacf0806fe161b
mzyist <mzyist@gmail.com>
2022-05-16 22:47:41 +0300

    Got rid of send_message function

--- send_message.py
+++ send_message.py
@@ -5,7 +5,12 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_message(chat_id, timer):
+if __name__ == "__main__":
+    load_dotenv()
+    telegram_token = os.getenv('TG_TOKEN')
+    timer = int(os.getenv('TIMER'))
+    chat_id = os.getenv('CHAT_ID')
+    bot = telegram.Bot(token=telegram_token)
     while True:
         for root, dir, files in os.walk('\images', topdown=True):
             for file in files:
@@ -14,12 +19,3 @@ def send_message(chat_id, timer):
                     photo=open(f'{root}\{file}', 'rb')
                 )
                 time.sleep(timer)
-
-
-if __name__ == "__main__":
-    load_dotenv()
-    telegram_token = os.getenv('TG_TOKEN')
-    timer = int(os.getenv('TIMER'))
-    chat_id = os.getenv('CHAT_ID')
-    bot = telegram.Bot(token=telegram_token)
-    send_message(chat_id, timer)
